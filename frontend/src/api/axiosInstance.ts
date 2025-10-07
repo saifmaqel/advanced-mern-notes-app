85a82827cf4f53711d1845b1e535e0cfc0b43f4a
sief aqel <sief.aqel@teklabz.com>
2025-10-07 09:07:40 +0300

    updated base url

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -23,7 +23,7 @@ export function scheduleTokenExpiryWatcher(expiresIn: number) {
 }
 
 export const api = axios.create({
-  baseURL: "https://technotes.onrender.com",
+  baseURL: "https://technotes-api-1a2h.onrender.com",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
